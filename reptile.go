4902aff66b9ddb4ca6d7ab7cfc3462f56983af7c
zhangcanzhao <zhangcanzhao@xlanlian.com>
2019-03-15 17:36:39 +0800

    golang

---
+++ reptile.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+const (
+	baseUrl string = "https://weekend.ctrip.com/around/shanghai/huodong/p"
+)
+
+func main() {
+
+	var page int = 1
+	var count int = getPageCount()
+
+	for {
+		str := baseUrl + strconv.Itoa(page)
+		response := getResponse(str)
+		if response.StatusCode == 403 {
+			fmt.Println("IP 已被禁止访问")
+			os.Exit(1)
+		}
+		if response.StatusCode == 200 {
+			dom, err := goquery.NewDocumentFromResponse(response)
+			if err != nil {
+				log.Fatalf("失败原因", response.StatusCode)
+			}
+			dom.Find(".basefix .searchresult_left").Each(func(i int, content *goquery.Selection) {
+				title := content.Find(".searchlocation_fun a h2 strong").Text()
+				// img := content.Find(".searchlocation_fun .product_pic img").Attr("src")
+				fmt.Printf(title, "\n")
+			})
+		}
+		page++
+		if page >= count {
+			fmt.Println("--------------------------------数据拉取完成共" + strconv.Itoa(page) + "条------------------------------------")
+			os.Exit(1)
+		}
+	}
+
+}
+
+/**
+* 返回response
+ */
+func getResponse(url string) *http.Response {
+	client := &http.Client{}
+	request, _ := http.NewRequest("GET", url, nil)
+	request.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:50.0) Gecko/20100101 Firefox/50.0")
+	response, _ := client.Do(request)
+	return response
+}
+
+/**
+* 得到文章总数
+ */
+func getPageCount() int {
+	response := getResponse(baseUrl)
+	dom, err := goquery.NewDocumentFromResponse(response)
+	if err != nil {
+		log.Fatalf("失败原因", response.StatusCode)
+	}
+	resDom := dom.Find(".pkg_page a")
+	//len := resDom.Length()
+	count, _ := strconv.Atoi(resDom.Eq(resDom.Length() - 2).Text())
+	return count
+}
